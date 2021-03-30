b528f8ec2050161fef177a43140e5be44451a4fa
german medina <german_medina_aponte@hotmail.com>
2021-03-30 11:51:03 -0400

    app de consola

---
+++ test1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Add Record </title>
+    <link rel="stylesheet" href="StyleSheet.css" />
+    <style type="text/css">
+        body {
+            background-image: while);
+        }
+    </style>
+</head>
+
+<body>
+</body>
+</html>
